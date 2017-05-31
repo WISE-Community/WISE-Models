6320fad1820a21ce1dd34dd6674d102b7bf6a816
Megan Nguyen <nguyenmegan@berkeley.edu>
2017-05-31 16:53:52 -0700

    starting chloroplast

---
+++ chloroplast/chloroplast.js
@@ -0,0 +1,328 @@
+// Global variables
+
+// the SVG.js draw object
+var draw = null;
+
+//answers - true: correct, false: incorrect
+var correct1 = true;
+var correct2 = true;
+var correct3 = true;
+var correct4 = true;
+var correct5 = true;
+var correct6 = true;
+
+//gray box
+var box1;
+var box2;
+var box3;
+var box4;
+var box5;
+var box6;
+
+//element currently in the boxes, respectively
+var element1;
+var element2;
+var element3;
+var element4;
+var element5;
+var element6;
+
+//elements
+var lightEnergy;
+var carbonDioxide;
+var oxygen;
+var glucose;
+var water;
+var soil;
+var chemicalEnergy;
+
+//number of tries
+var tries = 0;
+
+class Element {
+    constructor(x, y, image, text, textY) {
+        //original position
+        this.origX = x;
+        this.origY = y;
+
+        //current position
+        this.x = x;
+        this.y = y;
+
+        //draw element
+        this.image = draw.image(image, 120, 120);
+        this.text = draw.text(text).x(60).y(textY).font({size: 22, family: 'Roboto', anchor: 'middle', weight: 'bold'});
+        this.red = draw.image('./red.svg', 120, 120);
+        this.red.front().hide();
+
+        //create group
+        this.group = draw.group();
+        this.group.add(this.image);
+        this.group.add(this.text);
+        this.group.add(this.red);
+        this.group.x(x);
+        this.group.y(y);
+        this.group.addClass('pointer');
+
+        //drag n drop functionality
+        var self = this;
+        self.group.draggable(function(x, y) {
+            this.front();
+            return { x: x < 1060 && x > 0, y: y < 760 && y > 0}
+        }).on("dragend", function() {
+
+            //source -> target swapping and snapping
+            if (this.x() > (box1.x - 70)  && this.x() < (box1.x + 70)  && this.y() > (box1.y - 70) && this.y() < (box1.y + 70)) {
+                this.move(box1.x, box1.y);
+                if (element1 && element1 != self) {
+                    element1.group.front().animate(100, '-', 0).move(element1.origX, element1.origY);
+                }
+                element1 = self;
+            } else if (this.x() > (box2.x - 70)  && this.x() < (box2.x + 70)  && this.y() > (box2.y - 70) && this.y() < (box2.y + 70)) {
+                this.move(box2.x, box2.y);
+                if (element2 && element2 != self) {
+                    element2.group.front().animate(120, '-', 0).move(element2.origX, element2.origY);
+                }
+                element2 = self;
+            } else if (this.x() > (box3.x - 70)  && this.x() < (box3.x + 70)  && this.y() > (box3.y - 70) && this.y() < (box3.y + 70)) {
+                this.move(box3.x, box3.y);
+                if (element3 && element3 != self) {
+                    element3.group.front().animate(140, '-', 0).move(element3.origX, element3.origY);
+                }
+                element3 = self;
+            } else if (this.x() > (box4.x - 70)  && this.x() < (box4.x + 70)  && this.y() > (box4.y - 70) && this.y() < (box4.y + 70)) {
+                this.move(box4.x, box4.y);
+                if (element4 && element4 != self) {
+                    element4.group.front().animate(100, '-', 0).move(element4.origX, element4.origY);
+                }
+                element4 = self;
+            } else if (this.x() > (box5.x - 70)  && this.x() < (box5.x + 70)  && this.y() > (box5.y - 70) && this.y() < (box5.y + 70)) {
+                this.move(box5.x, box5.y);
+                if (element5 && element5 != self) {
+                    element5.group.front().animate(120, '-', 0).move(element5.origX, element5.origY);
+                }
+                element5 = self;
+            } else if (this.x() > (box6.x - 70)  && this.x() < (box6.x + 70)  && this.y() > (box6.y - 70) && this.y() < (box6.y + 70)) {
+                this.move(box6.x, box6.y);
+                if (element6 && element6 != self) {
+                    element6.group.front().animate(140, '-', 0).move(element6.origX, element6.origY);
+                }
+                element6 = self;
+            } else {
+                this.move(self.origX, self.origY);
+            }
+            //update current x and y position
+            self.x = this.x();
+            self.y = this.y();
+
+        }).on("dragstart", function() {
+            //set boxes to null if an element is removed
+            if (self == element1) {
+                element1 = null;
+            } else if (self == element2) {
+                element2 = null;
+            } else if (self == element3) {
+                element3 = null;
+            }
+        });
+    }
+}
+
+//create Box class
+class Box {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.image = draw.image('./box.svg', 120, 120).attr({
+            'x': x,
+            'y': y,
+        });
+    }
+}
+
+//makes red outline appear when cursor is over element
+function red(element) {
+    element.group.mouseover(function() {
+        element.red.show();
+    });
+    element.group.mouseout(function() {
+        element.red.hide();
+    });
+}
+
+function createInstructions() {
+    instructions = draw.text(function(add) {
+        add.tspan('Click and drag boxes into the gray boxes below to complete ')
+        add.tspan('a cycle of photosynthesis ').font({weight: 'bold'})
+        add.tspan('and cellular respiration.').font({weight: 'bold'}).newLine();
+        add.tspan('When you are ready to see if your answer is right, ')
+        add.tspan('click the \"check\" button.').newLine();
+    }).x(20).y(20).font({size: 21});
+}
+
+function createMitochondria() {
+    mitochondria = draw.image('./mitochondria.svg', 450, 350).attr({
+        'x': 550,
+        'y': 370,
+    });
+}
+
+function createChloroplast() {
+    chloroplast = draw.image('./chloroplast.svg', 380, 380).attr({
+        'x': 80,
+        'y': 340,
+    });
+}
+
+function createArrows() {
+    arrows = draw.image('./arrows.svg', 850, 300).attr({
+        'x': 160,
+        'y': 390,
+    });
+}
+
+//create the check button and its functionality
+function createCheck() {
+	//draw check button
+	checkGroup = draw.nested();
+	shadow = checkGroup.image('./shadow.svg', 270, 120).attr({
+		'x': 888,
+		'y': 8,
+	});
+	check = checkGroup.image('./check.svg', 230, 80).attr({
+		'x': 900,
+		'y': 15,
+	});
+	check2 = checkGroup.image('./check2.svg', 230, 80).attr({
+		'x': 900,
+		'y': 15,
+	});
+	check2.hide();
+	checkText = checkGroup.text('Check!').x(935).y(15).font({size: 48, family: 'Raleway', fill: '#FF9900'});
+	checkGroup.addClass('pointer');
+
+	//hover effect for check button
+	checkGroup.mouseover(function() {
+		check2.show();
+		checkText.font({fill: '#009999'});
+	})
+
+	//when check button is clicked
+	checkGroup.click(function() {
+		tries++;
+		//checkCorrect();
+		//successful attempt
+		if (correct1 && correct2 && correct3 && correct4 && correct5 && correct6) {
+			instructions.clear();
+			instructions.text = draw.text(function(add) {
+				add.tspan('Good job! ')
+				add.tspan('Photosynthesis ').font({weight: 'bold'})
+				add.tspan('stores chemical energy ').fill('#0ace00').font({weight: 'bold'})
+				add.tspan('in glucose. ')
+				add.tspan('Cellular respiration ').font({weight: 'bold'})
+				add.tspan('releases the stored chemical ').fill('#0000FF').font({weight: 'bold'})
+				add.tspan('energy from glucose. ').fill('#0000FF').font({weight: 'bold'}).newLine();
+				add.tspan('Notice that one chemical reaction creates the materials needed for the other.')
+				add.tspan('It\'s like a cycle!').newLine();
+			}).x(20).y(20).font({size: 21});
+
+			//sound effect
+			var audio = new Audio('./kid.mp3');
+			audio.play();
+
+			//hide all other images
+			shadow.hide();
+			check.hide();
+			check2.hide();
+			checkText.hide();
+			soil.group.hide();
+
+			//make correct answers undraggable
+			carbonDioxide.group.draggable(false);
+			water.group.draggable(false);
+            oxygen.group.draggable(false);
+            water.group.draggable(false);
+            glucose.group.draggable(false);
+            chemicalEnergy.group.draggable(false);
+
+		//unsuccessful attempt
+		} else if (tries < 3){
+			reset();
+			if (tries == 1) {
+				instructions.text('Try again. You have 2 more chances.');
+			}
+			if (tries == 2) {
+				instructions.text('Try again. You have 1 more chance.');
+			}
+
+		//unsuccessful attempts and out of tries
+		} else {
+			instructions.text('Nice try, but that is not correct. This was your last chance. \n Click the \"Try Again\" button to restart the quiz!');
+			shadow.hide();
+			check.hide();
+			check2.hide();
+			checkText.hide();
+			createTryButton();
+		}
+	})
+
+	//hover effect for check button
+	checkGroup.mouseout(function() {
+		check2.hide();
+		checkText.font({fill: '#FF9900'})
+	})
+}
+
+//create the try again button and its functionality
+function createTryButton() {
+	tryGroup = draw.nested();
+	tryButton = tryGroup.image('./try.svg', 160, 70).attr({
+		'x': 1020,
+		'y': 20,
+	});
+	tryText = tryGroup.text('Try Again').x(1037).y(33).font({size: 30, family: 'Raleway'});
+	tryGroup.click(function() {
+		location.reload();
+	})
+	tryGroup.addClass('pointer');
+}
+
+function init() {
+    // create the SVG.js draw object
+    draw = SVG('model');
+
+    //draw images
+    createMitochondria();
+    createChloroplast();
+    createArrows();
+
+    //create grey boxes
+    box1 = new Box(30, 320);
+    box2 = new Box(420, 320);
+    box3 = new Box(595, 320);
+    box4 = new Box(420, 640);
+    box5 = new Box(600, 640);
+    box6 = new Box(960, 660);
+
+    // //instructions text
+    createInstructions();
+
+    //create Check button
+    createCheck();
+
+    //create elements
+    lightEnergy = new Element(20, 135, './lightenergy.svg', 'Light \n Energy', 60);
+    carbonDioxide = new Element(165, 135, './carbonDioxide.svg', 'Carbon \n Dioxide', 60);
+    oxygen = new Element(310, 135, './oxygen.svg', 'Oxygen', 80);
+    glucose = new Element(455, 135, './glucose.svg', 'Glucose', 85);
+    water = new Element(600, 135, './water.svg', 'Water', 80);
+    soil = new Element(745, 135, './soil.svg', 'Soil', 80);
+    chemicalEnergy = new Element(890, 135, './chemicalEnergy.svg', 'Usable \n Chemical \n Energy', 25);
+
+    var elements = [lightEnergy, carbonDioxide, oxygen, glucose, water, soil, chemicalEnergy];
+
+    //apply red function to all elements -- see above
+    for (var i = 0; i < 8; i++) {
+        red(elements[i]);
+    }
+}
---
+++ chloroplast/dinosaur.js
@@ -0,0 +1,344 @@
+// Global variables
+
+// the SVG.js draw object
+var draw = null;
+
+//answers - true: correct, false: incorrect
+var correct1 = false;
+var correct2 = false;
+var correct3 = false;
+
+//boxes
+var box1;
+var box2;
+var box3;
+
+//elements currently in the boxes, respectively
+var element1;
+var element2;
+var element3;
+
+//elements
+var lightEnergy;
+var carbonDioxide;
+var water;
+var sugar;
+var oxygen;
+var soil;
+var heatEnergy;
+
+//number of tries
+var tries = 0;
+
+//create Element class
+class Element {
+	constructor(x, y, image, text, textY) {
+		//original position
+		this.origX = x;
+		this.origY = y;
+
+		//current position
+		this.x = x;
+		this.y = y;
+
+		//draw element
+		this.image = draw.image(image, 140, 140);
+		this.text = draw.text(text).x(70).y(textY).font({size: 24, family: 'Roboto', anchor: 'middle', weight: 'bold'});
+		this.red = draw.image('./red.svg', 140, 140);
+		this.red.front().hide();
+
+		//create group
+		this.group = draw.group();
+		this.group.add(this.image);
+		this.group.add(this.text);
+		this.group.add(this.red);
+		this.group.x(x);
+		this.group.y(y);
+		this.group.addClass('pointer');
+
+		//drag n drop functionality
+		var self = this;
+		self.group.draggable(function(x, y) {
+			this.front();
+			return { x: x < 1060 && x > 0, y: y < 760 && y > 0}
+		}).on("dragend", function() {
+
+			//source -> target swapping and snapping
+			if (this.x() > (box1.x - 70)  && this.x() < (box1.x + 70)  && this.y() > (box1.y - 70) && this.y() < (box1.y + 70)) {
+				this.move(box1.x, box1.y);
+				if (element1 && element1 != self) {
+					element1.group.front().animate(100, '-', 0).move(element1.origX, element1.origY);
+				}
+				element1 = self;
+			} else if (this.x() > (box2.x - 70)  && this.x() < (box2.x + 70)  && this.y() > (box2.y - 70) && this.y() < (box2.y + 70)) {
+				this.move(box2.x, box2.y);
+				if (element2 && element2 != self) {
+					element2.group.front().animate(120, '-', 0).move(element2.origX, element2.origY);
+				}
+				element2 = self;
+			} else if (this.x() > (box3.x - 70)  && this.x() < (box3.x + 70)  && this.y() > (box3.y - 70) && this.y() < (box3.y + 70)) {
+				this.move(box3.x, box3.y);
+				if (element3 && element3 != self) {
+					element3.group.front().animate(140, '-', 0).move(element3.origX, element3.origY);
+				}
+				element3 = self;
+			} else {
+				this.move(self.origX, self.origY);
+			}
+			//update current x and y position
+			self.x = this.x();
+			self.y = this.y();
+
+		}).on("dragstart", function() {
+			//set boxes to null if an element is removed
+			if (self == element1) {
+				element1 = null;
+			} else if (self == element2) {
+				element2 = null;
+			} else if (self == element3) {
+				element3 = null;
+			}
+		});
+	}
+}
+
+//create Box class
+class Box {
+	constructor(x, y) {
+		this.x = x;
+		this.y = y;
+		this.image = draw.image('./box.svg', 140, 140).attr({
+				'x': x,
+				'y': y,
+		});
+	}
+}
+
+//makes red outline appear when cursor is over element
+function red(element) {
+	element.group.mouseover(function() {
+		element.red.show();
+	});
+	element.group.mouseout(function() {
+		element.red.hide();
+	});
+}
+
+//create the background
+function createBackground() {
+	background = draw.image('./bg.svg', 1200, 900);
+}
+
+//create the tree
+function createTree() {
+	tree = draw.image('./tree.svg', 530, 470).attr({
+		'x': 450,
+		'y': 295,
+	});
+}
+
+//create the dinosaur
+function createDino() {
+	dino = draw.image('./dino.svg', 375, 375).attr({
+		'x': 820,
+		'y': 450,
+	});
+}
+
+//create the roots
+function createRoots() {
+	roots = draw.image('./roots.svg', 460, 520).attr({
+		'x': 385,
+		'y': 370,
+	});
+}
+
+//create the instructions text
+function createInstructions() {
+	instructions = draw.text('Click and drag the 3 necessary ingredients into the gray boxes below \n When you are ready to see if your answer is right, click the \"check\" button.').x(20).y(20).font({size: 28, family: 'Chewy'});
+}
+
+//create the check button and its functionality
+function createCheck() {
+	//draw check button
+	checkGroup = draw.nested();
+	shadow = checkGroup.image('./shadow.svg', 270, 120).attr({
+		'x': 888,
+		'y': 8,
+	});
+	check = checkGroup.image('./check.svg', 230, 80).attr({
+		'x': 900,
+		'y': 15,
+	});
+	check2 = checkGroup.image('./check2.svg', 230, 80).attr({
+		'x': 900,
+		'y': 15,
+	});
+	check2.hide();
+	checkText = checkGroup.text('Check!').x(935).y(15).font({size: 48, family: 'Raleway', fill: '#FF9900'});
+	checkGroup.addClass('pointer');
+
+	//hover effect for check button
+	checkGroup.mouseover(function() {
+		check2.show();
+		checkText.font({fill: '#009999'});
+	})
+
+	//when check button is clicked
+	checkGroup.click(function() {
+		tries++;
+		checkCorrect();
+
+		//successful attempt
+		if (correct1 && correct2 && correct3) {
+			instructions.clear();
+			instructions.text = draw.text(function(add) {
+				add.tspan('Great job! Plants need ')
+				add.tspan('light energy').fill('#ffb23f')
+				add.tspan(', ')
+				add.tspan('carbon dioxide').fill('#f06')
+				add.tspan(' and ')
+				add.tspan('water').fill('#4fd0ff')
+				add.tspan(' to make their own food (a type of ')
+				add.tspan('sugar)')
+				add.tspan('.')
+				add.tspan('Let\'s go to the next step and explore how plants get these elements!').newLine()
+			}).x(20).y(20).font({size: 28, family: 'Chewy'});
+			createFullTree();
+
+			//sound effect
+			var audio = new Audio('./kid.mp3');
+			audio.play();
+
+			//animate dinosaur
+			dino.animate(500, '<', 10).move(810, 415);
+			dino.animate(400, '-', 0).move(750, 420);
+			dino.animate(200, '-', 10).move(750, 430);
+			dino.animate(600, '<', 0).move(710, 450);
+
+			//hide all other images
+			background.hide();
+			shadow.hide();
+			check.hide();
+			check2.hide();
+			checkText.hide();
+			oxygen.group.hide();
+			sugar.group.hide();
+			soil.group.hide();
+			heatEnergy.group.hide();
+
+			//make correct answers undraggable
+			carbonDioxide.group.draggable(false);
+			lightEnergy.group.draggable(false);
+			water.group.draggable(false);
+
+		//unsuccessful attempt
+		} else if (tries < 3){
+			reset();
+			if (tries == 1) {
+				instructions.text('Try again. You have 2 more chances.');
+			}
+			if (tries == 2) {
+				instructions.text('Try again. You have 1 more chance.');
+			}
+
+		//unsuccessful attempts and out of tries
+		} else {
+			instructions.text('Nice try, but that is not correct. This was your last chance. \n Click the \"Try Again\" button to restart the quiz!');
+			shadow.hide();
+			check.hide();
+			check2.hide();
+			checkText.hide();
+			createTryButton();
+		}
+	})
+
+	//hover effect for check button
+	checkGroup.mouseout(function() {
+		check2.hide();
+		checkText.font({fill: '#FF9900'})
+	})
+}
+
+//create the try again button and its functionality
+function createTryButton() {
+	tryGroup = draw.nested();
+	tryButton = tryGroup.image('./try.svg', 160, 70).attr({
+		'x': 1020,
+		'y': 20,
+	});
+	tryText = tryGroup.text('Try Again').x(1037).y(33).font({size: 30, family: 'Raleway'});
+	tryGroup.click(function() {
+		location.reload();
+	})
+	tryGroup.addClass('pointer');
+}
+
+//create the tree that appears when the student is successful
+function createFullTree() {
+	fullTree = draw.image('./fulltree.svg', 1200, 900).back();
+}
+
+//reset incorrect ones
+function reset() {
+	if (!correct1 && element1 != null) {
+		element1.group.move(element1.origX, element1.origY);
+	}
+	if (!correct2 && element2 != null) {
+		element2.group.move(element2.origX, element2.origY);
+	}
+	if (!correct3 && element3 != null) {
+		element3.group.move(element3.origX, element3.origY);
+	}
+}
+
+//checks if the answers are correct
+function checkCorrect() {
+	if (element1 == lightEnergy || element1 == carbonDioxide) {
+		correct1 = true;
+	}
+	if (element2 == lightEnergy || element2 == carbonDioxide) {
+		correct2 = true;
+	}
+	if (element3 == water) {
+		correct3 = true;
+	}
+}
+
+function init() {
+	// create the SVG.js draw object
+	draw = SVG('model');
+
+	//draw images
+	createBackground();
+	createRoots();
+	createTree();
+	createDino();
+
+	//create grey boxes
+	box1 = new Box(270, 350);
+	box2 = new Box(270, 515);
+	box3 = new Box(345, 750);
+
+	//instructions text
+	createInstructions();
+
+	//create Check button
+	createCheck();
+
+	//create elements
+	lightEnergy = new Element(20, 135, './lightenergy.svg', 'Light \n Energy', 70);
+	carbonDioxide = new Element(165, 135, './carbonDioxide.svg', 'Carbon \n Dioxide', 70);
+ 	water = new Element(310, 135, './water.svg', 'Water', 90);
+	sugar = new Element(455, 135, './sugar.svg', 'Sugar', 100);
+	oxygen = new Element(600, 135, './oxygen.svg', 'Oxygen', 90);
+	soil = new Element(745, 135, './soil.svg', 'Soil', 90);
+	heatEnergy = new Element(890, 135, './heatenergy.svg', 'Heat \n Energy', 70);
+
+	var elements = [lightEnergy, carbonDioxide, water, sugar, oxygen, soil, heatEnergy];
+
+	//apply red function to all elements -- see above
+	for (var i = 0; i < 8; i++) {
+    	red(elements[i]);
+	}
+}
